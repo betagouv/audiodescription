2b5dd8b32647d74869d05ea70432b2d34cbdd493
Chloé Corfmat <contact.chloecorfmat@gmail.com>
2024-11-07 15:27:27 +0100

    feat: create director page

---
+++ drupal/web/modules/custom/audiodescription/src/Breadcrumb/DirectorBreadcrumbBuilder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Drupal\audiodescription\Breadcrumb;
+
+use Drupal\Core\Breadcrumb\Breadcrumb;
+use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\views\Views;
+
+/**
+ * Provides a breadcrumb builder for taxonomy terms directors.
+ */
+class DirectorBreadcrumbBuilder implements BreadcrumbBuilderInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function applies(RouteMatchInterface $route_match) {
+    $taxonomy_term = $route_match->getParameter('taxonomy_term');
+    return $taxonomy_term && $taxonomy_term->bundle() === 'director';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(RouteMatchInterface $route_match) {
+    $breadcrumb = new Breadcrumb();
+    $breadcrumb->addCacheContexts(['url', 'route']);
+
+    // Add the 'Home' link.
+    $breadcrumb->addLink(Link::createFromRoute(t('Home'), '<front>'));
+
+    $taxonomy_term = $route_match->getParameter('taxonomy_term');
+    $tid = $taxonomy_term->id();
+    $breadcrumb->addLink(Link::createFromRoute($taxonomy_term->getName(), 'entity.taxonomy_term.canonical', ['taxonomy_term' => $tid]));
+
+    return $breadcrumb;
+  }
+
+}
--- drupal/web/themes/ad_theme/lit/rich-select.js
+++ drupal/web/themes/ad_theme/lit/rich-select.js
@@ -1,48 +1,43 @@
-import { LitElement, html, css } from '/libraries/lit/lit-all.min.js';
+import { LitElement, html, css, classMap } from '/libraries/lit/lit-all.min.js';
 
 export class RichSelect extends LitElement {
   static properties = {
-    label: { type: String }
+    label: { type: String },
+    visible: { type: Boolean }
   };
 
   constructor() {
     super();
-    this.label = '';
+    this.visible = false;
   }
 
-  static styles = css`
-    :host {
-      color: seagreen;
-    }
-  `;
+  // Disable shadow DOM.
+  createRenderRoot() {
+    return this;
+  }
 
   render() {
+    const classes = {
+      'ad-dropdown': true,
+      'ad-dropdown--visible': this.visible
+    };
+
     return html`
       <div>
-        <button class="fr-btn"  data-fr-opened="false" aria-controls="fr-modal-3">
-          ${this.label}
+        <button type="button" class="fr-btn" data-fr-opened="false" aria-controls="ad-${this.label}" @click="${this._toggleDropdown}">
+          ${this.label} ${this.visible}
         </button>
-        <dialog aria-labelledby="fr-modal-title-modal-3" role="dialog" id="fr-modal-3" class="fr-modal">
-          <div class="rich-select-widget">
-            <div class="fr-container fr-container--fluid fr-container-md">
-              <div class="fr-grid-row fr-grid-row--center">
-                <div class="fr-col-12 fr-col-md-4">
-                  <div class="fr-modal__body">
-                    <div class="fr-modal__header">
-                      <button class="fr-btn--close fr-btn" title="Fermer la fenêtre modale" aria-controls="fr-modal-3">Fermer</button>
-                    </div>
-                    <div class="fr-modal__content">
-                      <h1 id="fr-modal-title-modal-3" class="fr-modal__title"><span class="fr-icon-arrow-right-line fr-icon--lg"></span>Titre de la modale SM</h1>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas varius tortor nibh, sit amet tempor nibh finibus et. Aenean eu enim justo. Vestibulum aliquam hendrerit molestie. Mauris malesuada nisi sit amet augue accumsan tincidunt. Maecenas tincidunt, velit ac porttitor pulvinar, tortor eros facilisis libero, vitae commodo nunc quam et ligula. Ut nec ipsum sapien. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer id nisi nec nulla luctus lacinia non eu turpis. Etiam in ex imperdiet justo tincidunt egestas. Ut porttitor urna ac augue cursus tincidunt sit amet sed orci.</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </dialog>
+        <div id="ad-${this.label}" class=${classMap(classes)} aria-hidden="${!this.visible}">
+          <h1>Coucou ${this.label}</h1>
+          <input type="text" name="prout[]" />
+        </div>
+
       </div>
     `;
+  }
+
+  _toggleDropdown(e) {
+    this.visible = !this.visible;
   }
 }
 
