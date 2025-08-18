780b025055b885e728717a8afbce574c2b00ec5b
Chloé Corfmat <contact.chloecorfmat@gmail.com>
2025-08-18 11:22:12 +0200

    fix: remove dump

--- patrimony/src/Service/MovieFetcher.php
+++ patrimony/src/Service/MovieFetcher.php
@@ -15,28 +15,18 @@ class MovieFetcher
   }
 
   public function fetchByIds($ids, SourceMovie $sourceMovie) {
-    // Récupérer le MovieRepository.
-    // findByIds
-    // En fonction du résultat :
-    // Je renvoie le film
-    // Je renvoie null
-    // Je merge les films et renvoie le movie
-
     $movies = $this->movieRepository->findByIds($ids, $sourceMovie->getCode());
 
     if (empty($movies)) {
-      dump('null');
       return NULL;
     }
 
     if (count($movies) == 1 ) {
-      dump('1');
       return $movies[0];
     }
 
     // Merge movies.
     if (count($movies) > 1) {
-      dump('multi');
       return $this->movieAutoMerger->autoMerge($ids, $movies, $sourceMovie);
     }
   }
