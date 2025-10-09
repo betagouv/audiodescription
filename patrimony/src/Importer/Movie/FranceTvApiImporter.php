1ca7c3883361f38100b9d348ed681ddc0916bab0
Chloé Corfmat <contact.chloecorfmat@gmail.com>
2025-10-09 10:48:29 +0200

    feat: create 2 requests for FTV

--- patrimony/src/Importer/Movie/FranceTvApiImporter.php
+++ patrimony/src/Importer/Movie/FranceTvApiImporter.php
@@ -18,6 +18,7 @@ use App\Service\MovieFetcher;
 use DateInterval;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
@@ -28,6 +29,8 @@ class FranceTvApiImporter implements MovieImporterInterface
 {
 
   private string $dataDir;
+  private EntityRepository $partnerRepository;
+  private EntityRepository $offerRepository;
 
   public function __construct(
     private EntityManagerInterface $entityManager,
@@ -40,10 +43,11 @@ class FranceTvApiImporter implements MovieImporterInterface
     private SolutionManager $solutionManager,
     private MovieManager $movieManager,
     private MovieFetcher $movieFetcher,
-
   )
   {
     $this->dataDir = sprintf('%s/data/', $this->parameterBag->get('kernel.project_dir'));
+    $this->partnerRepository = $this->entityManager->getRepository(Partner::class);
+    $this->offerRepository = $this->entityManager->getRepository(Offer::class);
   }
 
   /**
@@ -52,15 +56,10 @@ class FranceTvApiImporter implements MovieImporterInterface
   public function import(?array $options = []): void
   {
     // Manage options.
-    $createMoviesOption = $options['create-movies'];
-
-    $url = $this->parameterBag->get('france_tv.api.url');
-    $response = $this->httpClient->request('GET', $url);
-    $programs = $response->toArray();
+    $createMoviesOption = $options['create-movies'] ?? false;
 
     // Create partner France TV if not exists.
-    $partnerRepository = $this->entityManager->getRepository(Partner::class);
-    $partner = $partnerRepository->findOneBy(['code' => PartnerCode::FRANCE_TV->value]);
+    $partner = $this->partnerRepository->findOneBy(['code' => PartnerCode::FRANCE_TV->value]);
 
     if (is_null($partner)) {
       $partner = new Partner();
@@ -70,8 +69,7 @@ class FranceTvApiImporter implements MovieImporterInterface
     }
 
     // Create offer FREE_ACCESS if not exists.
-    $offerRepository = $this->entityManager->getRepository(Offer::class);
-    $offer = $offerRepository->findOneBy(['code' => OfferCode::FREE_ACCESS->value]);
+    $offer = $this->offerRepository->findOneBy(['code' => OfferCode::FREE_ACCESS->value]);
 
     if (is_null($offer)) {
       $offer = new Offer();
@@ -86,214 +84,226 @@ class FranceTvApiImporter implements MovieImporterInterface
 
     $this->entityManager->flush();
 
-    foreach ($programs as $program) {
-      $isTelefilm = 0;
+    $url = $this->parameterBag->get('france_tv_100_num.api.url');
+    $this->requestAndImport($url, $createMoviesOption);
 
-      // Genres.
-      $genres = [];
-      foreach ($program['tags'] as $tag) {
-        if ($tag['type'] == 'genre') {
-          $genres[] = $tag['label'];
-        }
+    $url = $this->parameterBag->get('france_tv_premium.api.url');
+    $this->requestAndImport($url, $createMoviesOption);
+  }
 
-        if ($tag['label'] == "téléfilms") {
-          $isTelefilm = TRUE;
-        }
+  private function importProgram($program, $createMoviesOption) {
+    $isTelefilm = 0;
+
+    // Genres.
+    $genres = [];
+    foreach ($program['tags'] as $tag) {
+      if ($tag['type'] == 'genre') {
+        $genres[] = $tag['label'];
       }
 
-      $startRights = null;
-      $endRights = null;
-
-      if(!empty($program['platforms']['ftv']['exploitation_windows'])) {
-        $startRights = $program['platforms']['ftv']['exploitation_windows'][0]['start'] ?? null;
-        $endRights = $program['platforms']['ftv']['exploitation_windows'][0]['end']?? null;
+      if ($tag['label'] == "téléfilms") {
+        $isTelefilm = TRUE;
       }
+    }
 
-      if (!is_null($startRights) && !is_null($endRights)) {
-        $dateStartRights = new DateTime($startRights);
-        $dateEndRights = new DateTime($endRights);
-        $today = new DateTime();
+    $startRights = null;
+    $endRights = null;
 
-        if (
-          !$isTelefilm
-          && ($dateStartRights <= $today)
-          && ($dateEndRights > $today)
-          && !empty($program['deep_links_v2'])
-        ) {
-          //@TODO : catch Throwable + Write a log + send Mattermost notif
-          $partner = $partnerRepository->findOneBy(['code' => PartnerCode::FRANCE_TV->value]);
-          $offer = $offerRepository->findOneBy(['code' => OfferCode::FREE_ACCESS->value]);
+    if(!empty($program['platforms']['ftv']['exploitation_windows'])) {
+      $startRights = $program['platforms']['ftv']['exploitation_windows'][0]['start'] ?? null;
+      $endRights = $program['platforms']['ftv']['exploitation_windows'][0]['end']?? null;
+    }
 
-          $internalPartnerId = $program['program_id'];
+    if (!is_null($startRights) && !is_null($endRights)) {
+      $dateStartRights = new DateTime($startRights);
+      $dateEndRights = new DateTime($endRights);
+      $today = new DateTime();
 
-          $repository = $this->entityManager->getRepository(SourceMovie::class);
-          $sourceMovie = $repository->findOneBy([
-            'internalPartnerId' => $internalPartnerId,
-            'partner' => $partner,
-          ]);
+      if (
+        !$isTelefilm
+        && ($dateStartRights <= $today)
+        && ($dateEndRights > $today)
+        && !empty($program['deep_links_v2'])
+      ) {
+        //@TODO : catch Throwable + Write a log + send Mattermost notif
+        $partner = $this->partnerRepository->findOneBy(['code' => PartnerCode::FRANCE_TV->value]);
+        $offer = $this->offerRepository->findOneBy(['code' => OfferCode::FREE_ACCESS->value]);
 
-          $title = $program['title'];
-          dump($title);
+        $internalPartnerId = $program['program_id'];
 
-          $productionYear = (int) $program['produced_at'];
+        $repository = $this->entityManager->getRepository(SourceMovie::class);
+        $sourceMovie = $repository->findOneBy([
+          'internalPartnerId' => $internalPartnerId,
+          'partner' => $partner,
+        ]);
 
-          if (is_null($sourceMovie)) {
-            $sourceMovie = $this->sourceMovieManager->findOrcreate(
-              $title,
-              $internalPartnerId,
-              $productionYear,
-              $partner,
-              TRUE
-            );
+        $title = $program['title'];
+        dump($title);
+
+        $productionYear = (int) $program['produced_at'];
+
+        if (is_null($sourceMovie)) {
+          $sourceMovie = $this->sourceMovieManager->findOrcreate(
+            $title,
+            $internalPartnerId,
+            $productionYear,
+            $partner,
+            TRUE
+          );
+        }
+
+        $directors = [];
+        $casting = [];
+        foreach ($program['credits'] as $credit) {
+          switch ($credit['role']['id']) {
+            case 'realisateur':
+              if (isset($credit['first_name']) && !is_null($credit['first_name'])) {
+                $directors[] = [
+                  'fullname' => $credit['first_name'] . ' ' . $credit['last_name'],
+                ];
+              } else {
+                $directors[] = [
+                  'fullname' => $credit['last_name'],
+                ];
+              }
+              break;
+            case 'acteur':
+              if (isset($credit['first_name']) && !is_null($credit['first_name'])) {
+                $casting[] = [
+                  'fullname' => $credit['first_name'] . ' ' . $credit['last_name'],
+                ];
+              } else {
+                $casting[] = [
+                  'fullname' => $credit['last_name'],
+                ];
+              }
+              break;
+            default:
+              break;
           }
+        }
 
-          $directors = [];
-          $casting = [];
-          foreach ($program['credits'] as $credit) {
-            switch ($credit['role']['id']) {
-              case 'realisateur':
-                if (isset($credit['first_name']) && !is_null($credit['first_name'])) {
-                  $directors[] = [
-                    'fullname' => $credit['first_name'] . ' ' . $credit['last_name'],
-                  ];
-                } else {
-                  $directors[] = [
-                    'fullname' => $credit['last_name'],
-                  ];
-                }
-                break;
-              case 'acteur':
-                if (isset($credit['first_name']) && !is_null($credit['first_name'])) {
-                  $casting[] = [
-                    'fullname' => $credit['first_name'] . ' ' . $credit['last_name'],
-                  ];
-                } else {
-                  $casting[] = [
-                    'fullname' => $credit['last_name'],
-                  ];
-                }
-                break;
-              default:
-                break;
-            }
+        $sourceMovie->setDirectors($directors);
+        $sourceMovie->setCasting($casting);
+
+        $synopsis = html_entity_decode(strip_tags($program['description']));
+        $sourceMovie->setSynopsis($synopsis);
+
+        // Public.
+        if (isset($program['rating']) && !is_null($program['rating'])) {
+          switch ($program['rating']) {
+            case 'deconseille-aux-10-ans':
+              // Non recommandé avant 10 ans.
+              $sourceMovie->setPublic('10');
+              break;
+            case 'deconseille-aux-12-ans':
+              // Non recommandé avant 12 ans.
+              $sourceMovie->setPublic('12');
+              break;
+            case 'deconseille-aux-16-ans':
+              // Non recommandé avant 16 ans.
+              $sourceMovie->setPublic('16');
+              break;
+            case 'deconseille-aux-18-ans':
+              // Non recommandé avant 18 ans.
+              $sourceMovie->setPublic('18');
+              break;
+            default:
+              break;
           }
+        }
 
-          $sourceMovie->setDirectors($directors);
-          $sourceMovie->setCasting($casting);
+        if (isset($program['duration']) && !is_null($program['duration'])) {
+          $duration = $program['duration'];
+        } else {
+          $duration = $program['expected_duration'];
+        }
+        $interval = new DateInterval($duration);
+        $totalMinutes = ($interval->h * 60) + $interval->i + ($interval->s / 60);
+        $sourceMovie->setDuration((int) $totalMinutes);
 
-          $synopsis = html_entity_decode(strip_tags($program['description']));
-          $sourceMovie->setSynopsis($synopsis);
-
-          // Public.
-          if (isset($program['rating']) && !is_null($program['rating'])) {
-            switch ($program['rating']) {
-              case 'deconseille-aux-10-ans':
-                // Non recommandé avant 10 ans.
-                $sourceMovie->setPublic('10');
-                break;
-              case 'deconseille-aux-12-ans':
-                // Non recommandé avant 12 ans.
-                $sourceMovie->setPublic('12');
-                break;
-              case 'deconseille-aux-16-ans':
-                // Non recommandé avant 16 ans.
-                $sourceMovie->setPublic('16');
-                break;
-              case 'deconseille-aux-18-ans':
-                // Non recommandé avant 18 ans.
-                $sourceMovie->setPublic('18');
-                break;
-              default:
-                break;
-            }
+        $poster = '';
+        foreach ($program['images'] as $image) {
+          if ($image['ratio'] == '3:4') {
+            $poster = $image['url'];
           }
+        }
+        $sourceMovie->setPoster($poster);
 
-          if (isset($program['duration']) && !is_null($program['duration'])) {
-            $duration = $program['duration'];
-          } else {
-            $duration = $program['expected_duration'];
-          }
-          $interval = new DateInterval($duration);
-          $totalMinutes = ($interval->h * 60) + $interval->i + ($interval->s / 60);
-          $sourceMovie->setDuration((int) $totalMinutes);
+        $sourceMovie->setGenres($genres);
 
-          $poster = '';
-          foreach ($program['images'] as $image) {
-            if ($image['ratio'] == '3:4') {
-              $poster = $image['url'];
-            }
-          }
-          $sourceMovie->setPoster($poster);
+        $this->entityManager->persist($sourceMovie);
 
-          $sourceMovie->setGenres($genres);
+        // ExternalIds
 
-          $this->entityManager->persist($sourceMovie);
+        $ids = [];
+        $ids['franceTvId'] = $internalPartnerId;
 
-          // ExternalIds
+        if (isset($program['imdb']['episode_id']) && !is_null($program['imdb']['episode_id'])) {
+          $ids['imdbId'] = $program['imdb']['episode_id'];
+        }
 
-          $ids = [];
-          $ids['franceTvId'] = $internalPartnerId;
+        if (isset($program['plurimedia_broadcast_id']) && !is_null($program['plurimedia_broadcast_id'])) {
+          $ids['plurimediaId'] = $program['plurimedia_broadcast_id'];
+        }
 
-          if (isset($program['imdb']['episode_id']) && !is_null($program['imdb']['episode_id'])) {
-            $ids['imdbId'] = $program['imdb']['episode_id'];
-          }
+        if (isset($program['allocine']['movie_id']) && !is_null($program['allocine']['movie_id'])) {
+          $ids['allocineId'] = $program['allocine']['movie_id'];
+        }
 
-          if (isset($program['plurimedia_broadcast_id']) && !is_null($program['plurimedia_broadcast_id'])) {
-            $ids['plurimediaId'] = $program['plurimedia_broadcast_id'];
-          }
+        //dump($ids);
 
-          if (isset($program['allocine']['movie_id']) && !is_null($program['allocine']['movie_id'])) {
-            $ids['allocineId'] = $program['allocine']['movie_id'];
-          }
-
-          //dump($ids);
-
-          $link = '';
-          foreach ($program['deep_links_v2'] as $deepLink) {
-            if ($deepLink['type'] == 'Google') {
-              foreach ($deepLink['targets'] as $target) {
-                if ($target['offer'] == 'ftv') {
-                  if (in_array('androidTV', $target['platforms'])) {
-                    $link = $target['url'];
-                  }
+        $link = '';
+        foreach ($program['deep_links_v2'] as $deepLink) {
+          if ($deepLink['type'] == 'Google') {
+            foreach ($deepLink['targets'] as $target) {
+              if ($target['offer'] == 'ftv') {
+                if (in_array('androidTV', $target['platforms'])) {
+                  $link = $target['url'];
                 }
               }
             }
           }
+        }
 
-          $solution = $this->solutionManager->createOrUpdate(
-            $internalPartnerId,
-            $partner,
-            $sourceMovie,
-            $offer,
-            $link,
-            $startRights,
-            $endRights,
-          );
+        $solution = $this->solutionManager->createOrUpdate(
+          $internalPartnerId,
+          $partner,
+          $sourceMovie,
+          $offer,
+          $link,
+          $startRights,
+          $endRights,
+        );
 
-          $this->entityManager->persist($solution);
+        $this->entityManager->persist($solution);
 
-          $movie = null;
-          if ($createMoviesOption) {
-            //$movie = $this->movieRepository->findByIds($ids, $sourceMovie->getCode());
+        $movie = null;
+        if ($createMoviesOption) {
+          //$movie = $this->movieRepository->findByIds($ids, $sourceMovie->getCode());
 
-            $movie = $this->movieFetcher->fetchByIds($ids, $sourceMovie);
+          $movie = $this->movieFetcher->fetchByIds($ids, $sourceMovie);
 
-            if (is_null($movie)) {
-              $movie = $this->movieManager->create($sourceMovie);
-              $this->entityManager->persist($movie);
-            }
-
-            $sourceMovie->setMovie($movie);
-            $solution->setMovie($movie);
+          if (is_null($movie)) {
+            $movie = $this->movieManager->create($sourceMovie);
+            $this->entityManager->persist($movie);
           }
 
-          $this->entityManager->flush();
-          $this->entityManager->clear();
+          $sourceMovie->setMovie($movie);
+          $solution->setMovie($movie);
         }
+
+        $this->entityManager->flush();
+        $this->entityManager->clear();
       }
+    }
+  }
 
-
+  private function requestAndImport($url, $createMoviesOption) {
+    $response = $this->httpClient->request('GET', $url);
+    $programs = $response->toArray();
+    foreach ($programs as $program) {
+      $this->importProgram($program, $createMoviesOption);
     }
   }
 }
